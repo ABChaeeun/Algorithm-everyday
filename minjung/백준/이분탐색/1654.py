ed4c73c7dce1c2edd98c09b367e10ae79e0a8889
minjung-sys <cho9906@naver.com>
2022-10-31 15:12:54 +0900

    [1654] 랜선 자르기 (BOJ/Silver2)

--- minjung/백준/이분탐색/1654.py
+++ minjung/백준/이분탐색/1654.py
@@ -1,24 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(data):
-    start = 1
-    end = max(data)
-    total = 0
+def binary_search(start,end,data):
+    result = 0
     while start <= end:
         mid = (start + end) //2
+        total = 0
         for lan in data :
             total += lan // mid
         if total >= N :
             start = mid + 1
-            return mid
+            result = mid
         else :
             end = mid - 1
-
+    return result
 K , N = map(int,input().split())
 
-data = []
-for i in range(K):
-    data.append(int(input()))
+data=[int(input()) for _ in range(K)]
+start = 1
+end = max(data)
+print(binary_search(start,end,data))
 
-print(binary_search(data))
+
