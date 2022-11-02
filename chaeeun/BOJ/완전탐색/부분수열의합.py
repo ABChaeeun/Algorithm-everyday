8c7171b26c85138fb8142a57bb4f2fbb82239cbb
chaeeun <abcodmss@naver.com>
2022-11-02 15:57:48 +0900

    [1182] 부분수열의 합

---
+++ chaeeun/BOJ/완전탐색/부분수열의합.py
@@ -0,0 +1,14 @@
+import itertools
+
+N, S = map(int, input().split())
+lst = list(map(int, input().split()))
+
+cnt = 0
+for count in range(1, N+1):
+    nCr = itertools.combinations(lst, count)
+    # print(list(nCr))
+    for num in nCr:
+        if sum(num) == S:
+            cnt += 1
+
+print(cnt)
