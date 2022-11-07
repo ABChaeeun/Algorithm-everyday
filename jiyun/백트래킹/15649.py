30e4b768ab530d505fd3af703ad9a457ced402d3
namjiyun <namjiyun3275@gmail.com>
2022-11-07 21:53:15 +0900

    [15652] n과m(BOJ/Silver3)

---
+++ jiyun/백트래킹/15649.py
@@ -0,0 +1,17 @@
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+arr = []
+
+def dfs():
+    if(len(arr) == m):
+        print(' '.join(map(str, arr)))
+    else:
+        for i in range(1, n+1):
+            if(i not in arr):
+                arr.append(i)
+                dfs()
+                arr.pop()
+    
+dfs()
