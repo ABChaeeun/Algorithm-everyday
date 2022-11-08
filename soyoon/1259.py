71fcbe9a0204c80c980a59926d967021efb7329b
sxyxxnk <soyoon0215@sookmyung.ac.kr>
2022-11-08 17:31:11 +0900

    [5430] AC

--- soyoon/1259.py
+++ soyoon/1259.py
@@ -1,11 +1,5 @@
 import math
 
-# def length(n):
-#     if len(n) % 2 == 0:
-#         return len(n)/2
-#     elif len(n) % 2 == 1:
-#         return int(len(n)/2)+1
-
 while True:
     num = input()
     count = 0
@@ -25,5 +19,5 @@ while True:
                     break
         if count == 0:
             print("yes")
-            
+
 
