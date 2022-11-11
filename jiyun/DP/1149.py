9f27596b20e9ed23fc8351638eeac87851759d6b
namjiyun <namjiyun3275@gmail.com>
2022-11-11 17:26:52 +0900

    [1149] RGB거리 (BOJ/Silver1)

---
+++ jiyun/DP/1149.py
@@ -0,0 +1,14 @@
+import sys
+n = int(input())
+
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, n):
+    arr[i][0] = min(arr[i-1][1] + arr[i][0], arr[i][0] + arr[i-1][2])
+    arr[i][1] = min(arr[i-1][0] + arr[i][1], arr[i][1] + arr[i-1][2])
+    arr[i][2] = min(arr[i-1][0] + arr[i][2], arr[i][2] + arr[i-1][1])
+    
+print(min(arr[n-1][0], arr[n-1][1], arr[n-1][2]))
