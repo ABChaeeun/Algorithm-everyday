4b9028b32fa8cb64f264573c46cfe41bf175b54b
sxyxxnk <soyoon0215@sookmyung.ac.kr>
2022-11-13 19:05:58 +0900

    [2743] 단어 길이 재기

--- soyoon/1931.py
+++ soyoon/1931.py
@@ -1,9 +1,22 @@
-n = int(input())
-discussion_time = [[0 for j in range(2)] for i in range(n)]
+import sys
+
+n = int(sys.stdin.readline())
+
+discussion_time = [[0]*2 for i in range(n)]
 
 for i in range(n):
-    start, end = map(int, input().split())
+    start, end = map(int, sys.stdin.readline().split())
     discussion_time[i][0] = start
     discussion_time[i][1] = end
 
-print(discussion_time)
+discussion_time.sort(key=lambda x:(x[1], x[0]))
+
+end_time = discussion_time[0][1]
+cnt = 1
+
+for j in range(1, n):
+    if discussion_time[j][0] >= end_time:
+        cnt += 1
+        end_time = discussion_time[j][1]
+
+print(cnt)
